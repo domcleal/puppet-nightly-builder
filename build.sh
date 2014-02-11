9fa62c8e73236f39efe0a8cd48964d9d21f0afdf
Dominic Cleal <dcleal@redhat.com>
2014-02-11 20:39:01 +0000

    Initial commit, builds puppet + facter on copr

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -xe
+
+ssh -o "BatchMode yes" people.redhat.com "rm public_html/puppet-nightly/*" || true
+
+for d in puppet facter; do
+  if [ -e $d ]; then
+    (cd $d && git pull)
+  else
+    git clone https://github.com/puppetlabs/$d
+  fi
+
+  pushd $d
+  [ -e pkg/srpm ] && rm -rf pkg/srpm
+  rake package:bootstrap
+  rake package:srpm
+  scp -o "BatchMode yes" pkg/srpm/*.rpm people.redhat.com:public_html/puppet-nightly/
+  for f in pkg/srpm/*.rpm; do
+    copr-cli build puppet-nightly \
+      http://people.redhat.com/~dcleal/puppet-nightly/$(basename $f)
+  done
+  popd
+done
